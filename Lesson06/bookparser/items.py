6c0c9ac5eab5c4625bb74d8224310404839270fe
Alexey <alkaz01@bk.ru>
2021-09-05 14:07:23 +0300

    Lesson06 added

---
+++ Lesson06/bookparser/items.py
@@ -0,0 +1,17 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BookparserItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field()
+    url = scrapy.Field()
+    authors = scrapy.Field()
+    std_price = scrapy.Field()
+    sale_price = scrapy.Field()
+    rating = scrapy.Field()
+    _id = scrapy.Field()
